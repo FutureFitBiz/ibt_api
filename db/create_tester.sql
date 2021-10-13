987307c195cf05d28aa3ea5e7e6373d98cacb1d6
Geoff Kendall <geoff+github@geoffkendall.com>
2021-10-13 16:25:42 +0100

    Update create_tester.sql

--- db/create_tester.sql
+++ db/create_tester.sql
@@ -1,3 +1,4 @@
 use ibt;
-insert into user(email,password)
-values ( 'tester@futurefitbusiness.org', '$2b$12$FGxUfHcVWTQGOKaA2HQQn.T2cEMt.1q2vf2XtIYrP2XWw/bwjEY62' );
+delete from user where email='tester@futurefitbusiness.org';
+insert into user(first, email, password, admin)
+values ( 'Tester', 'tester@futurefitbusiness.org', '$2b$12$FGxUfHcVWTQGOKaA2HQQn.T2cEMt.1q2vf2XtIYrP2XWw/bwjEY62', true);
